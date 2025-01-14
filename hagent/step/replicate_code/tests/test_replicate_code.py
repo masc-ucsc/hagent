b3e1cf7934902ed40ca5a3d7edc69415cbb369f5
Jose Renau <renau@ucsc.edu>
2025-01-13 17:40:59 -0800

    add new test to replicate_code 100%

--- hagent/step/replicate_code/tests/test_replicate_code.py
+++ hagent/step/replicate_code/tests/test_replicate_code.py
@@ -1,7 +1,22 @@
 # See LICENSE for details
 
 import os
+import pytest
+
 from hagent.step.replicate_code.replicate_code import Replicate_code
+
+
+def test_replicate_missing_llm():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+
+    inp_file = os.path.join(test_dir, 'bad_input.yaml')
+
+    trivial_step = Replicate_code()
+    trivial_step.set_io(inp_file=inp_file, out_file='test_replicate_code_output.yaml')
+
+    with pytest.raises(ValueError):
+        trivial_step.setup()
+        trivial_step.step()
 
 
 def test_replicate_code():
@@ -23,4 +38,5 @@ def test_replicate_code():
 
 
 if __name__ == '__main__':  # pragma: no cover
+    test_replicate_missing_llm()
     test_replicate_code()
