7059a4e5a9d0b21fa08fbd0e7070b8b51d6df7f7
Jose Renau <renau@ucsc.edu>
2025-02-24 16:38:00 -0800

    support environment variables for yaml passes

--- hagent/core/step.py
+++ hagent/core/step.py
@@ -1,18 +1,38 @@
 # See LICENSE for details
+# See LICENSE for details
 
 import sys
-import yaml
 import datetime
+import os
+import contextlib
+
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import LiteralScalarString
 
 from hagent.core.llm_wrap import dict_deep_merge
+
+
+def wrap_literals(obj):
+    # Recursively wrap multiline strings as LiteralScalarString for nicer YAML output.
+    if isinstance(obj, dict):
+        return {k: wrap_literals(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [wrap_literals(elem) for elem in obj]
+    elif isinstance(obj, str) and '\n' in obj:
+        return LiteralScalarString(obj)
+    else:
+        return obj
+
 
 class Step:
     def __init__(self):
         self.input_file = None
         self.output_file = None
+        self.overwrite_conf = {}
         self.setup_called = False
+        self.input_data = None
 
-    def set_io(self, inp_file: str, out_file: str, overwrite_conf: dict={}):
+    def set_io(self, inp_file: str, out_file: str, overwrite_conf: dict = {}):
         self.input_file = inp_file
         self.output_file = out_file
         self.overwrite_conf = overwrite_conf
@@ -22,7 +42,6 @@ class Step:
         self.output_file = None
 
         args = sys.argv[1:]
-
         i = 0
         while i < len(args):
             if args[i].startswith('-o'):
@@ -45,41 +64,50 @@ class Step:
             sys.exit(1)
 
     def read_input(self):
-        data = {}
+        # Read input using ruamel.yaml for improved formatting.
         if self.input_file is None:
             return {'error': f'{sys.argv[0]} {datetime.datetime.now().isoformat()} - unset input_file (missing setup?):'}
-
         try:
+            yaml_obj = YAML(typ='safe')
             with open(self.input_file, 'r') as f:
-                data = yaml.safe_load(f)
+                data = yaml_obj.load(f)
         except Exception as e:
             return {'error': f'{sys.argv[0]} {datetime.datetime.now().isoformat()} - Error loading input file: {e}'}
 
         return data
 
     def write_output(self, data):
-        def write_yaml_with_block_style(data, file):
-            class LiteralStr(str):
-                pass
-
-            def represent_literal_str(dumper, value):
-                return dumper.represent_scalar('tag:yaml.org,2002:str', value, style='|')
-
-            def process_multiline_strings(obj):
-                if isinstance(obj, dict):
-                    return {k: process_multiline_strings(v) for k, v in obj.items()}
-                elif isinstance(obj, list):
-                    return [process_multiline_strings(i) for i in obj]
-                elif isinstance(obj, str) and '\n' in obj:
-                    return LiteralStr(obj)
-                return obj
-
-            yaml.add_representer(LiteralStr, represent_literal_str)
-            processed_data = process_multiline_strings(data)
-            yaml.dump(processed_data, file, default_flow_style=False, sort_keys=False)
-
+        # Write output using ruamel.yaml with wrapped literals.
+        yaml_obj = YAML()
+        yaml_obj.default_flow_style = False
+        processed_data = wrap_literals(data)
         with open(self.output_file, 'w') as f:
-            write_yaml_with_block_style(data, f)
+            yaml_obj.dump(processed_data, f)
+
+    @contextlib.contextmanager
+    def temporary_env_vars(self):
+        """
+        Context manager to temporarily set environment variables from input_data.
+        Expected format in YAML:
+          set_env_vars:
+            POTATO: "foo"
+            ANOTHER_VAR: "another_value"
+        Alternatively, if provided as a list with a single mapping, the mapping is used.
+        """
+        env_vars = self.input_data.get('set_env_vars', {})
+
+        original_env = {}
+        try:
+            for key, value in env_vars.items():
+                original_env[key] = os.environ.get(key)
+                os.environ[key] = value  # Set temporary environment variable
+            yield
+        finally:
+            for key in env_vars.keys():
+                if original_env.get(key) is None:
+                    os.environ.pop(key, None)  # Unset if it did not exist originally
+                else:
+                    os.environ[key] = original_env[key]  # Restore original value
 
     def setup(self):
         self.setup_called = True
@@ -88,65 +116,65 @@ class Step:
 
         if self.input_file:
             self.input_data = self.read_input()
-            if 'error' in self.input_data:
-                # Error occurred during reading input, write output_data as is
+            env_vars = self.input_data.get('set_env_vars', {})
+            if env_vars and not isinstance(env_vars, dict):
+                self.error('set_env_vars must be a map in yaml')
+                return
+
+            if isinstance(self.input_data, dict) and 'error' in self.input_data:
+                # Propagate error from input reading and exit
                 print('WARNING: error field in input yaml, just propagating')
                 self.write_output(self.input_data)
-                exit(4)
+                sys.exit(4)
             if self.overwrite_conf:
                 self.input_data = dict_deep_merge(self.input_data, self.overwrite_conf)
         else:
             self.input_data = self.overwrite_conf
 
     def run(self, data):
-        # To be implemented in the subclass
+        # To be implemented in the subclass.
         raise NotImplementedError('Subclasses should implement this!')
 
     def test(self, exp_file):
-        expected_output_yaml = exp_file
-        expected_output = {}
-        with open(expected_output_yaml, 'r') as f:
-            expected_output = yaml.safe_load(f)
-
+        # Unit test that compares run output against an expected YAML file.
+        yaml_obj = YAML(typ='safe')
+        with open(exp_file, 'r') as f:
+            expected_output = yaml_obj.load(f)
         assert expected_output is not None
         assert expected_output != {}
 
         self.input_data = self.read_input()
         self.setup()
-        result_data = self.run(self.input_data)
+        with self.temporary_env_vars():
+            result_data = self.run(self.input_data)
         if result_data != expected_output:
-            print(f'input_data:{self.input_data}')
-            print(f'result_data:{result_data}')
-            print(f'expect_data:{expected_output}')
-
+            print(f'input_data: {self.input_data}')
+            print(f'result_data: {result_data}')
+            print(f'expect_data: {expected_output}')
         assert result_data == expected_output
 
     def error(self, msg: str):
-        if self.input_data is None:
-            output_data = {}
-        else:
-            output_data = self.input_data.copy()
-
+        # Write error details to output and raise an exception.
+        output_data = self.input_data.copy() if self.input_data else {}
         output_data.update({'error': f'{sys.argv[0]} {datetime.datetime.now().isoformat()} {msg}'})
         print(f'ERROR: {sys.argv[0]} : {msg}')
-        self.write_output(self.input_data)
+        self.write_output(output_data)
         raise ValueError(msg)
 
     def step(self):
         if not self.setup_called:
             raise NotImplementedError('must call setup before step')
-
         output_data = {}
         try:
-            result_data = self.run(self.input_data)
+            # Set environment variables temporarily before running.
+            with self.temporary_env_vars():
+                result_data = self.run(self.input_data)
             if result_data is None:
                 result_data = {}
-            # Propagate all fields from input to output unless overridden
+            # Propagate all fields from input to output unless overridden.
             output_data.update(result_data)
         except Exception as e:
-            # Handle exceptions during run
             output_data.update({'error': f'{sys.argv[0]} {datetime.datetime.now().isoformat()} - unable to write yaml: {e}'})
             print(f'ERROR: unable to write yaml: {e}')
         self.write_output(output_data)
-
         return output_data
