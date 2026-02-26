8a4f1d425e3ca2e10c4589117db685fd5ceae1fd
Sri Senthil <ssenthi7@ucsc.edu>
2026-02-26 10:08:36 -0800

    made blink example more generic for compatiblity

--- examples/Blink/Blink/Blink.ino
+++ examples/Blink/Blink/Blink.ino
@@ -1,39 +1,18 @@
 /*
-  Nano R4 RGB Cycle
-  Cycles the built-in RGB LED through Red, Green, and Blue.
+  Blink
+  Blinks the built-in LED once per second.
+  Works on any Arduino board.
 */
 
 #include "Arduino.h"
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  // RGB LEDs on Nano R4 are active LOW (LOW = ON)
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
-  
-  // Start with all LEDs OFF (HIGH = OFF)
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, HIGH);
 }
 
 void loop() {
-  // RED
-  digitalWrite(LEDR, LOW);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-
-  // GREEN
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, LOW);
-  digitalWrite(LEDB, HIGH);
-  delay(1000);
-
-  // BLUE
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 }
